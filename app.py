9f2e18c5f16514add12b962aeeb037afe1e46fae
kavinraj2k <kavinraj2k@gmail.com>
2024-04-13 12:02:48 +0530

    some changes in app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route("/app")
+def app():
+    return "Hello"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=5000)
