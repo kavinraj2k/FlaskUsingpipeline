2b934894ed46f5ebc344db898a21d223075a1b8e
kavinraj2k <166891701+kavinraj2k@users.noreply.github.com>
2024-06-20 17:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, Response,render_template
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+import random
+import time
 
 app = Flask(__name__, template_folder='templates')
+
+    
+requests_total = Counter('requests_total', 'Total number of requests received')
+
 
 @app.route('/')
 def index():
@@ -9,6 +16,10 @@ def index():
 @app.route("/app")
 def app_route():
     return render_template('app.html')
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
